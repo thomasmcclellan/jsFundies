c20df99f040d4ad6f09233940e2ff1f9799a92c8
McClellan <Tom.McClellan@trimedx.com>
2019-04-29 08:47:25 -0400

    01.09

---
+++ 01-Arrays/09-entries.js
@@ -0,0 +1,28 @@
+//* 4/29/2019
+
+let array1 = ['a', 'b', 'c']
+let iterator1 = array1.entries()
+
+console.log(iterator1.next().value) // Array [0, 'a']
+console.log(iterator1.next().value) // Array [1, 'b']
+
+/*
+Syntax:
+array.entries()
+
+Return value:
+A new --Array-- iterator object
+*/
+
+const a = ['a', 'b', 'c']
+
+for (const [index, element] of a.entries())
+    console.log(index, element) // [0, 'a'] [1, 'b'] [2, 'c']
+
+let iterator = a.entries()
+
+for (let e of iterator) {
+    console.log(e) // [0, 'a'] [1, 'b'] [2, 'c']
+}
+
+//? https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/entries

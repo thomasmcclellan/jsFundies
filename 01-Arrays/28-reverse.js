1f929d1ecee67d7f982046544d97520a9f25acc2
McClellan <Tom.McClellan@trimedx.com>
2019-05-24 11:41:43 -0400

    01.26-28

--- 01-Arrays/28-reverse.js
+++ 01-Arrays/28-reverse.js
@@ -1,14 +1,37 @@
-//* 5//2019
+//* 5/24/2019
 
+const arr1 = ['one', 'two', 'three']
 
+console.log(arr1) // ['one', 'two', 'three']
 
+const reversed = arr1.reverse()
+
+console.log(reversed) // ['three', 'two', 'one']
+
+//* BE AWARE: Existing array mutated
+console.log(arr1) // ['three', 'two', 'one']
+ 
 /*
 Syntax:
-
+arr.reverse()
 
 Return value:
-
+The reversed array
 */
+
+//* Reversing the elements in an array-like object
+const arrObj = {
+  0: 'First',
+  1: 'Second',
+  2: 'Third',
+  length: 3
+}
+
+console.log(arrObj) // { 0: 'First', 1: 'Second', 2: 'Third', length: 3 }
+
+Array.prototype.reverse.call(arrObj) //* same syntax for using apply()
+
+console.log(arrObj) // { 0: 'Third', 1: 'Second', 2: 'First', length: 3 }
 
 /*
 Browser Compatibility:

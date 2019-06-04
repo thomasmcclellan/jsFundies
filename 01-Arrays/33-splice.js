3a89682891b914f73f259628fa09ef00966b8a64
McClellan <Tom.McClellan@trimedx.com>
2019-06-04 10:07:30 -0400

    01.33-34

--- 01-Arrays/33-splice.js
+++ 01-Arrays/33-splice.js
@@ -1,13 +1,26 @@
-//* 5//2019
+//* 6/03/2019
 
+const months = ['Jan', 'March', 'April', 'June']
+
+months.splice(1, 0, 'Feb')
+
+console.log(months) // ['Jan', 'Feb', 'March', 'April', 'June']
+
+months.splice(4, 0, 'May')
+
+console.log(months) // ['Jan', 'Feb', 'March', 'April', 'May', 'June']
 
 
 /*
 Syntax:
+arr.splice(start[, deleteCount[, item1[, item2[, ...]]]])
 
+start: index at which to start changing the array. If --> arr.length--, --start-- will be set to the length of the array.  If negative, it will begin that any eleents from the end of the array (with origin -1 meaning -n is the indx of the nth last element and istherefore equivalent to the index of --arr.length - n--).  If the absolute value of --start > arr.length--, it will begin from index 0.
+deleteCount (optional): integer indicating the number of elements in the array to remove from --start--.
+item1, item2, ... (optional): elements to add to the array, beginning from --start--.  If no elements are specified, --splice-- will only remove element from the array.
 
 Return value:
-
+An array containing the deleted elements.  If only one element is removed, an array of one element is returned.  If no elements are removed, an empty array is returned.
 */
 
 /*
--- 01-Arrays/34-toLocaleString.js
+++ 01-Arrays/34-toLocaleString.js
@@ -1,20 +1,39 @@
-//* 5//2019
+//* 6/04/2019
 
+const array1 = [1, 'a', new Date('21 Dec 1997 14:12:00 UTC')]
+const localeString = array1.toLocaleString('en', { timeZone: 'UTC' })
 
+console.log(localeString) // '1, a, 12/21/1997, 2:12:00 PM'
 
 /*
 Syntax:
+arr.toLocaleString([locales[, options]])
 
+locales (optional): string with a BCP 47 language tag, or an array of such strings.  For the general form and interpretation of the --locales-- argument
+options (optional): object with configuration properties
 
 Return value:
-
+A string representing the elements of the array
 */
 
 /*
 Browser Compatibility:
+Generally:
 Full Support: All
 Compatibility Unknown: NA
 No Support: NA
+
+With --locales-- paremeter:
+Full Support: Firefox
+Compatibility Unknown: All others
+No Support: Firefox for Android
+
+With --options-- paremeter:
+Full Support: Firefox
+Compatibility Unknown: All others
+No Support: 
+  Android webview
+  Firefox for Android
 */
 
 //? https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/toLocaleString
